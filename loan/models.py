f60bdf1967049b776f56637d161fa32152c27e20
amoloye <adedamolaadegoke2@gmail.com>
2023-03-12 00:51:42 +0200

    added api.py file to define the endpoints and their corresponding functions using the Ninja web framework. It contains the logic for handling HTTP requests and responses, as well as validation and data manipulation. it defines two endpoints: apply_loan and list_loans

--- loan/models.py
+++ loan/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from decimal import Decimal
 
 
 class Loan(models.Model):
@@ -7,3 +8,11 @@ class Loan(models.Model):
     name = models.CharField(max_length=255)
     personal_id = models.CharField(max_length=20)
     created_at = models.DateTimeField(auto_now_add=True)
+    monthly_repayment_amount = models.DecimalField(max_digits=10, decimal_places=2, default=Decimal(0.00))
+    interest_rate = models.DecimalField(max_digits=5, decimal_places=2, default=Decimal(0.00))
+
+    def save(self, *args, **kwargs):
+        # Calculate the monthly repayment amount based on the amount, term and interest rate
+        self.monthly_repayment_amount = Decimal(
+            (self.amount * self.interest_rate / 1200) / (1 - (1 + self.interest_rate / 1200) ** (-self.term)))
+        super().save(*args, **kwargs)
